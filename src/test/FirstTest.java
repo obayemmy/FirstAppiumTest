756a9bcb4b1c520b5dcacf5e4a3899a4cd9f88c1
Obayemmy <obayemmy92@gmail.com>
2020-02-27 13:44:11 +0100

    Added testNg

--- src/test/FirstTest.java
+++ src/test/FirstTest.java
@@ -1,16 +1,21 @@
 package test;
 
 import java.net.MalformedURLException;
+
 import java.net.URL;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeTest;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
+
 public class FirstTest {
-	public static void main(String[] args) throws MalformedURLException, InterruptedException {
+	public AndroidDriver<WebElement> driver;
+	@BeforeTest
+	public void launchBrowser() throws InterruptedException, MalformedURLException {
 		DesiredCapabilities dc = new DesiredCapabilities();
 		dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
 		dc.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
@@ -18,9 +23,13 @@ public class FirstTest {
 		dc.setCapability(MobileCapabilityType.DEVICE_NAME,"Android Emulator");
 		dc.setCapability(MobileCapabilityType.APP, "C:\\Users\\Agromall-PC\\Downloads\\AgroCola_V1.0.8.apk");
 		URL url = new URL("http://127.0.0.1:4723/wd/hub");
-		AndroidDriver<WebElement> driver = new AndroidDriver<WebElement>(url, dc);
+		driver = new AndroidDriver<WebElement>(url, dc);
 		Thread.sleep(8000);
 		System.out.println("Agrocola app launched successfully");
+		
+	}
+	
+	public void closeApp() {
 		driver.quit();
 	}
 
